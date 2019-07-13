eace2c22f70e4588fc605c00058bba4ec3947deb
glosskc <glosskc@gmail.com>
2019-07-13 16:21:54 -0500

    GBooks api added

--- google-search/Client/src/Components/Nav.js
+++ google-search/Client/src/Components/Nav.js
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 
 function Nav() {
     return (
-        <nav>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <h2>Google Books</h2>
             <Link to="/">Search</Link>
             <Link to="/saved">Saved</Link>
