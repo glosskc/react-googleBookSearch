c58a47d9bbb1dfedc6c8002b1d251ab78fd0c200
glosskc <glosskc@gmail.com>
2019-07-28 18:16:33 -0500

    added switch

--- google-search/Client/src/App.js
+++ google-search/Client/src/App.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
+import Search from "./Pages/search";
+import Saved from "./Pages/saved";
+import Nav from "./Components/Nav";
+import Header from "./Components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component {
+render() {
+  return(
+    <Router>
+      <div>
+        <Nav />
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Search} />
+          <Route exact path="/saved" component={Saved} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
+}
+
 
 export default App;
